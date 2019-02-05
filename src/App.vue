cdf7e02f11b5faa738e24fbc487f3f645914dc6a
Camilo Barrios Villamizar <malditagaseosa@gmail.com>
2019-02-05 01:13:18 -0500

    background image

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
-    <app-header v-bind:title="title" v-bind:repository="repository"></app-header>
-    <app-main></app-main>
+    <b-card>
+      <app-header v-bind:title="title" v-bind:repository="repository"></app-header>
+      <app-main></app-main>
+    </b-card>
   </div>
 </template>
 
@@ -28,7 +30,16 @@ export default {
   @import url('https://fonts.googleapis.com/css?family=Roboto');
 
   #app {
+    width: 70%;
     font-family: 'Roboto', sans-serif;
     align-content: center;
+    margin: 10% auto 0 auto;
   }
+
+  .card {
+    -webkit-box-shadow: -3px 4px 13px -4px rgba(105,101,105,0.93);
+    -moz-box-shadow: -3px 4px 13px -4px rgba(105,101,105,0.93);
+    box-shadow: -3px 4px 13px -4px rgba(105,101,105,0.93);
+  }
+  
 </style>
