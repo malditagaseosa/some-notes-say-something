ca1cc56724c6c341fd9c96866afd82d7068efcd0
Camilo Barrios Villamizar <malditagaseosa@gmail.com>
2019-02-05 00:38:07 -0500

    initial commit

--- src/services/convert-to-notes.js
+++ src/services/convert-to-notes.js
@@ -8,7 +8,11 @@ import Tone from "tone";
 const synth = new Tone.Synth().toMaster();
 
 //Default notes to use to convert the letters
-const DEFAULT_NOTES = ["C", "D", "E", "F", "G", "A", "B", "C"];
+const DEFAULT_NOTES = ["C", "D", "E", "F", "G", "A", "B", "C","C4", "D4", "E4",
+                      "F4", "G4", "A4", "B4", "C4","C7","D7", "E7", "F7", "G7", 
+                      "A7", "B7", "C75","C5","D5","E5"];
+const ALPHABET = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','Ñ',
+                  'O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 
 export default {
   /**
@@ -19,6 +23,7 @@ export default {
    * @returns {String}
    */
   convert: function(message) {
+    /*
     let notesMessage, unicodeMessage;
 
     unicodeMessage = message
@@ -41,14 +46,24 @@ export default {
     });
 
     return notesMessage.join("");
+    */
+    let notes = message.split("").map(currentValue => {
+      return DEFAULT_NOTES[ALPHABET.indexOf(currentValue.toUpperCase())];
+    }).join("");
+
+
+    return notes;
 
   },
   play: function(notes){
+    /*
     let pattern = new Tone.Pattern((time, note)=>{
       console.log(note);
       synth.triggerAttackRelease(note + "4", 1, time);
     }, notes);
 
     pattern.start(0);
+    */
+    console.log(ALPHABET.length, DEFAULT_NOTES.length);
   }
 };
